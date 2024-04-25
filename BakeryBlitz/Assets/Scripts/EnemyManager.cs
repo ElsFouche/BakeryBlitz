ba1dc824da69940c3032b14a989983c2048675aa
ElsFouche <166535453+ElsFouche@users.noreply.github.com>
2024-04-24 18:47:17 -0700

    Initial Setup

---
+++ BakeryBlitz/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Author: 
+ * Last Update:
+ * Notes:       This script handles enemy spawn rates,
+ *              spawn type, wave count, etc.
+ */
+
+public class EnemyManager : MonoBehaviour
+{
+
+}
