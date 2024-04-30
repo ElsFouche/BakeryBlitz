1caf6e5c4670bfff2a5adf9e9b27726de100fd47
sbelcher333 <marguriette@icloud.com>
2024-04-30 11:50:19 -0700

    player

--- BakeryBlitz/Assets/Scripts/PlayerController.cs
+++ BakeryBlitz/Assets/Scripts/PlayerController.cs
@@ -3,21 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 /*
  * Symon Belcher
- * 4/25/2024
+ * 4/30/2024
  * handles player movment throuout the map
  */
 
+
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector3 playerPos = new Vector3(0,0,0);
+
+    public float playerMove = 2;
+
+    public GameObject barriorPointL;
+    public GameObject barriorPointR;
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerPos = gameObject.transform.position; // determies the players postion
+
+        if (Input.GetKeyDown(KeyCode.D) && playerPos.x < barriorPointR.transform.position.x)//moves player right
+        {
+            playerPos.x += playerMove;
+
+            gameObject.transform.position = playerPos;
+
+        }
+        if (Input.GetKeyDown(KeyCode.A) && playerPos.x > barriorPointR.transform.position.x)// moves player left
+        {
+            playerPos.x -= playerMove;
+
+            gameObject.transform.position = playerPos;
+
+        }
+        if (Input.GetKeyDown(KeyCode.W) && playerPos.x < barriorPointR.transform.position.z)//moves player up
+        {
+            playerPos.z += playerMove;
+
+            gameObject.transform.position = playerPos;
+
+        }
+        if (Input.GetKeyDown(KeyCode.S) && playerPos.x > barriorPointR.transform.position.z)// moves player down
+        {
+            playerPos.z -= playerMove;
+
+            gameObject.transform.position = playerPos;
+
+        }
     }
+
 }
--- BakeryBlitz/Assets/Scripts/UIManager.cs
+++ BakeryBlitz/Assets/Scripts/UIManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEditor.Experimental.GraphView;
 /*
  * 4/25/2024
  * Symon Belcher
@@ -12,6 +13,12 @@ public class UIManager : MonoBehaviour
 
     public TMP_Text healthText;
     public TMP_Text resourceText;
+
+    public GameObject upPoint;
+    public GameObject downPoint;
+
+    private Vector3 leftPos;
+    private Vector3 rightPos;
 
     //public GameObject 
 
@@ -24,6 +31,27 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 moveDir;
+
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            
+           gameObject.transform.position = upPoint.transform.position;
+          
+        }
+        else if (Input.GetKey(KeyCode.Alpha2))
+        {
+
+           gameObject.transform.position = downPoint.transform.position;
+
+        }
+        
+         
+      
+        
+
+      
+
         
     }
 }
